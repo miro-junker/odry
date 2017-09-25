a7baf0ab46de4c5c5100db6e95c9187bb4318221
miro-junker <miro.junker@gmail.com>
2017-09-26 01:55:47 +0200

    model

--- schoolapp/admin.py
+++ schoolapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import News, Event, Gallery, Page
 
-# Register your models here.
+admin.site.register(News)
+admin.site.register(Event)
+admin.site.register(Gallery)
+admin.site.register(Page)
--- schoolapp/models.py
+++ schoolapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField(default=timezone.now)
+    perex = models.TextField()
+    content = models.TextField()
+    picture = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    since = models.DateTimeField()
+    to = models.DateTimeField()
+    info = models.TextField()
+    picture = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Gallery(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField(default=timezone.now)
+    picture = models.CharField(max_length=200)
+    path = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
